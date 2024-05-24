5998172bd09d7b5774d283b64d4a4c6d8b995722
SayedAli <sayedxali7@gmail.com>
2024-05-24 21:12:54 +0330

    enhance: modified swagger documentation for project

--- src/main/java/com/seyed/ali/authenticationservice/config/swagger/SwaggerConfiguration.java
+++ src/main/java/com/seyed/ali/authenticationservice/config/swagger/SwaggerConfiguration.java
@@ -1,7 +1,10 @@
 package com.seyed.ali.authenticationservice.config.swagger;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +15,19 @@ import org.springframework.context.annotation.Configuration;
         scheme = "bearer",
         type = SecuritySchemeType.OPENIDCONNECT,
         in = SecuritySchemeIn.HEADER
+)
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Authentication",
+                version = "0.1",
+                summary = "Summary :: Authentication-Service microservice.",
+                description = "Description :: Authentication-Service microservice.",
+                contact = @Contact(
+                        name = "SeyedAli",
+                        email = "seyedali.devl@gmail.com",
+                        url = "https://github.com/seyedali-dev"
+                )
+        )
 )
 public class SwaggerConfiguration {
 }
